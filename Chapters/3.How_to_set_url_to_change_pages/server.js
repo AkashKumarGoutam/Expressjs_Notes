b1a1c5b20cae718f23c84c2452fa49b005719188
AkashKumarGoutam <akashkumar126954@gmail.com>
2024-07-25 19:37:03 +0530

    add some topics

---
+++ Chapters/3.How_to_set_url_to_change_pages/server.js
@@ -0,0 +1,21 @@
+// How to change page url by the help of Express js
+
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/' , (req , res) =>{
+    res.sendFile(path.join(__dirname) + '/home.html')
+})
+
+
+app.get('/about' , (req , res) =>{
+    res.sendFile(path.join(__dirname) + '/about.html')
+})
+
+app.listen(PORT , ()=>{
+    console.log(`Listening Port No ${PORT}`)
+})
